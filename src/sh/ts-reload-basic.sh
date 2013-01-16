3463d0f687124fc8ba7f07397a7c6605e6d2760a
Esmé Cowles <escowles@ucsd.edu>
2013-01-16 10:02:39 -0500

    Updating script to use clearer ARK variable names
    Former-commit-id: 01099158014939c21e5629401539fa0277252754

--- src/sh/ts-reload-basic.sh
+++ src/sh/ts-reload-basic.sh
@@ -30,31 +30,31 @@ $BASE/ts-clear.sh $ES
 $BASE/ts-load.sh $ES src/sample/predicates/
 
 # create a sample repository
-ARK1=`mintark`
-JSON=`echo "[{\"subject\":\"$ARK1\",\"predicate\":\"dams:repositoryName\",\"object\":\"\\\\\"Test Repository 2\\\\\"\"},{\"subject\":\"$ARK1\",\"predicate\":\"rdf:type\",\"object\":\"dams:Repository\"},{\"subject\":\"$ARK1\",\"predicate\":\"dams:repositoryURI\",\"object\":\"http://test.com\"}]" | urlencode`
-curl -X POST "http://localhost:8080/dams/api/objects/$ARK1?adds=$JSON"
+REP_ARK=`mintark`
+JSON=`echo "[{\"subject\":\"$REP_ARK\",\"predicate\":\"dams:repositoryName\",\"object\":\"\\\\\"Test Repository 2\\\\\"\"},{\"subject\":\"$REP_ARK\",\"predicate\":\"rdf:type\",\"object\":\"dams:Repository\"},{\"subject\":\"$REP_ARK\",\"predicate\":\"dams:repositoryURI\",\"object\":\"http://test.com\"}]" | urlencode`
+curl -X POST "http://localhost:8080/dams/api/objects/$REP_ARK?adds=$JSON"
 echo
 
 # create a sample collection
-ARK2=`mintark`
-JSON=`echo "[{\"subject\":\"$ARK2\",\"predicate\":\"dams:title\",\"object\":\"node1\"},{\"subject\":\"node1\",\"predicate\":\"rdf:type\",\"object\":\"dams:Title\"},{\"subject\":\"node1\",\"predicate\":\"rdf:value\",\"object\":\"\\\\\"Test Collection\\\\\"\"},{\"subject\":\"$ARK2\",\"predicate\":\"rdf:type\",\"object\":\"dams:Collection\"}]" | urlencode`
-curl -X POST "http://localhost:8080/dams/api/objects/$ARK2?adds=$JSON"
+COL_ARK=`mintark`
+JSON=`echo "[{\"subject\":\"$COL_ARK\",\"predicate\":\"dams:title\",\"object\":\"node1\"},{\"subject\":\"node1\",\"predicate\":\"rdf:type\",\"object\":\"dams:Title\"},{\"subject\":\"node1\",\"predicate\":\"rdf:value\",\"object\":\"\\\\\"Test Collection\\\\\"\"},{\"subject\":\"$COL_ARK\",\"predicate\":\"rdf:type\",\"object\":\"dams:Collection\"}]" | urlencode`
+curl -X POST "http://localhost:8080/dams/api/objects/$COL_ARK?adds=$JSON"
 echo
 
 # create a sample object
-ARK3=`mintark`
-JSON=`echo "[{\"subject\":\"$ARK3\",\"predicate\":\"dams:title\",\"object\":\"node1\"},{\"subject\":\"node1\",\"predicate\":\"rdf:type\",\"object\":\"dams:Title\"},{\"subject\":\"node1\",\"predicate\":\"rdf:value\",\"object\":\"\\\\\"Test Object\\\\\"\"},{\"subject\":\"$ARK3\",\"predicate\":\"rdf:type\",\"object\":\"dams:Object\"},{\"subject\":\"$ARK3\",\"predicate\":\"dams:repository\",\"object\":\"$DAMSID$ARK1\"},{\"subject\":\"$ARK3\",\"predicate\":\"dams:collection\",\"object\":\"$DAMSID$ARK2\"}]" | urlencode`
-curl -X POST "http://localhost:8080/dams/api/objects/$ARK3?adds=$JSON"
+OBJ_ARK=`mintark`
+JSON=`echo "[{\"subject\":\"$OBJ_ARK\",\"predicate\":\"dams:title\",\"object\":\"node1\"},{\"subject\":\"node1\",\"predicate\":\"rdf:type\",\"object\":\"dams:Title\"},{\"subject\":\"node1\",\"predicate\":\"rdf:value\",\"object\":\"\\\\\"Test Object\\\\\"\"},{\"subject\":\"$OBJ_ARK\",\"predicate\":\"rdf:type\",\"object\":\"dams:Object\"},{\"subject\":\"$OBJ_ARK\",\"predicate\":\"dams:repository\",\"object\":\"$DAMSID$REP_ARK\"},{\"subject\":\"$OBJ_ARK\",\"predicate\":\"dams:collection\",\"object\":\"$DAMSID$COL_ARK\"}]" | urlencode`
+curl -X POST "http://localhost:8080/dams/api/objects/$OBJ_ARK?adds=$JSON"
 echo
 
 # attach a file to the object
 FILE=$HOME/src/dams/src/sample/files/20775-bb01010101-1-1.tif
 SRCPATH=`dirname $FILE`
 echo $FILE
-curl -i -X POST -F sourcePath="$SRCPATH" -F file=@$FILE http://localhost:8080/dams/api/files/$ARK3/1.tif
+curl -i -X POST -F sourcePath="$SRCPATH" -F file=@$FILE http://localhost:8080/dams/api/files/$OBJ_ARK/1.tif
 echo
 
 # generate derivatives
 SIZES="-F size=2 -F size=3 -F size=4 -F size=5"
-curl -i -L -X POST $SIZES http://localhost:8080/dams/api/files/$ARK3/1.tif/derivatives
+curl -i -L -X POST $SIZES http://localhost:8080/dams/api/files/$OBJ_ARK/1.tif/derivatives
 echo
