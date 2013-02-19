d66f961f34eb1dd2128a4e1b15625bb134e908e3
Esmé Cowles <escowles@ucsd.edu>
2013-02-19 15:38:05 -0500

    Updating hierarchical complex object sample
    Former-commit-id: 72b4d8d056c3b0046eab29a5f4392b78e2c1614a

--- src/sh/test_XX_complex2.sh
+++ src/sh/test_XX_complex2.sh
@@ -21,12 +21,20 @@ $BASE/ts-delete.sh $ARK
 $BASE/fs-delete.sh $ARK 1/1.pdf
 $BASE/fs-delete.sh $ARK 3/1.jpg
 $BASE/fs-delete.sh $ARK 4/1.jpg
-for i in 1 3 4; do
+$BASE/fs-delete.sh $ARK 5/1.keep
+$BASE/fs-delete.sh $ARK 6/1.xlsx
+$BASE/fs-delete.sh $ARK 7/1.keep
+$BASE/fs-delete.sh $ARK 8/1.xlsx
+$BASE/fs-delete.sh $ARK 9/1.keep
+$BASE/fs-delete.sh $ARK 10/1.png
+$BASE/fs-delete.sh $ARK 11/1.png
+for i in 1 3 4 10 11; do
 	for j in 2 3 4 5; do
 		echo $i/$j
 		$BASE/fs-delete.sh $ARK $i/$j.jpg
 	done
 done
+read -p "Press any key to continue... "
 
 # post first files
 $BASE/fs-post-cmp.sh $ARK 1 1.pdf src/sample/files/20775-bb01034796-1-1.pdf
@@ -37,9 +45,17 @@ $BASE/ts-put.sh $ARK src/sample/object/damsComplexObject2.rdf.xml
 # post subcomponent files after metadata scaffold in place
 $BASE/fs-post-cmp.sh $ARK 3 1.jpg src/sample/files/20775-bb75097630-1-1.jpg
 $BASE/fs-post-cmp.sh $ARK 4 1.jpg src/sample/files/20775-bb01010101-2-1.jpg
+$BASE/fs-post-cmp.sh $ARK 5 1.keep src/sample/files/damsComplexObject2-1.keep
+$BASE/fs-post-cmp.sh $ARK 6 1.xslx src/sample/files/damsComplexObject2-1.xslx 
+$BASE/fs-post-cmp.sh $ARK 7 1.keep src/sample/files/damsComplexObject2-1.keep
+$BASE/fs-post-cmp.sh $ARK 8 1.xslx src/sample/files/damsComplexObject2-2.xslx
+$BASE/fs-post-cmp.sh $ARK 9 1.keep src/sample/files/damsComplexObject2-1.keep
+$BASE/fs-post-cmp.sh $ARK 10 1.png src/sample/files/damsComplexObject2-1.png
+$BASE/fs-post-cmp.sh $ARK 11 1.png src/sample/files/damsComplexObject2-2.png
 
 # generate derivatives
 $BASE/fs-derivatives-cmp.sh $ARK 1 1.pdf
 $BASE/fs-derivatives-cmp.sh $ARK 3 1.jpg
 $BASE/fs-derivatives-cmp.sh $ARK 4 1.jpg
-
+$BASE/fs-derivatives-cmp.sh $ARK 10 1.png
+$BASE/fs-derivatives-cmp.sh $ARK 11 1.png
