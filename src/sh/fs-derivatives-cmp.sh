9cedacb533b6bfdc221827fef56f6a655ff118e9
Esmé Cowles <escowles@ucsd.edu>
2013-02-26 14:16:19 -0500

    Adding new derivative sizes to scripts
    Former-commit-id: 91046b337950d0514a4e2ea7dd899978ab184f2a

--- src/sh/fs-derivatives-cmp.sh
+++ src/sh/fs-derivatives-cmp.sh
@@ -5,8 +5,7 @@
 OBJID=$1
 CMPID=$2
 FILEID=$3
-SIZES="-F size=2 -F size=3 -F size=4 -F size=5"
-curl -i -L -X POST $SIZES http://localhost:8080/dams/api/files/$OBJID/$CMPID/$FILEID/derivatives
+curl -i -L -X POST "http://localhost:8080/dams/api/files/$OBJID/$CMPID/$FILEID/derivatives?size=2&size=3&size=4&size=5&size=6&size=7"
 if [ $? != 0 ]; then
     exit 1
 fi
--- src/sh/fs-derivatives.sh
+++ src/sh/fs-derivatives.sh
@@ -4,8 +4,7 @@
 
 OBJID=$1
 FILEID=$2
-SIZES="-F size=2 -F size=3 -F size=4 -F size=5"
-curl -i -L -X POST $SIZES http://localhost:8080/dams/api/files/$OBJID/$FILEID/derivatives
+curl -i -L -X POST "http://localhost:8080/dams/api/files/$OBJID/$FILEID/derivatives?size=2&size=3&size=4&size=5&size=6&size=7"
 if [ $? != 0 ]; then
     exit 1
 fi
