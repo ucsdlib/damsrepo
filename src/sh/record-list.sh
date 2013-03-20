0c5e8edbbdcd9a19d0da62960e37eb5b660b6729
Esmé Cowles <escowles@ucsd.edu>
2013-03-20 07:24:04 -0400

    Adding optional parameter to record-list to make it easier to list records of a given type
    Former-commit-id: ea11c3ad25400b5f38b7526963df009dc62cb348

--- src/sh/record-list.sh
+++ src/sh/record-list.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -o tmp/records.xml http://localhost:8080/dams/api/records
+curl -o tmp/records.xml http://localhost:8080/dams/api/records/$1
 xsltproc src/xsl/record-list.xsl tmp/records.xml
