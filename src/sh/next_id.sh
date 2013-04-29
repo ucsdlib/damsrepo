4f91ed5957a65937d1259b000b22cba650b0739a
Esmé Cowles <escowles@ucsd.edu>
2013-04-29 17:10:51 -0400

    Updating next_id.sh to use auth, adding new sample records
    Former-commit-id: 81883b3b2bbc9c23a439be2aebabffa94b0b08dd

--- src/sh/next_id.sh
+++ src/sh/next_id.sh
@@ -2,7 +2,13 @@
 
 # generate a new identifier
 
-curl -X POST http://localhost:8080/dams/api/next_id
+BASE=`dirname $0`
+source $BASE/common.sh
+
+if [ "$1" ]; then
+	COUNT="?count=$1"
+fi
+curl -X POST -u $USER:$PASS http://localhost:8080/dams/api/next_id$COUNT
 if [ $? != 0 ]; then
     exit 1
 fi
