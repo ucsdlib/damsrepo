95bd540edfa6dad135be8c056838377974b871dd
Esmé Cowles <escowles@ucsd.edu>
2013-10-15 09:30:44 -0400

    Sample data scripts now assume empty state, don't delete existing records
    Former-commit-id: 4e796f31ca3edafe9021ba0678aab132c07f5606

--- src/sh/test_XX_formats.sh
+++ src/sh/test_XX_formats.sh
@@ -5,42 +5,25 @@
 BASE=`dirname $0`
 
 # audio (wav master, mp3 service)
-$BASE/fs-delete.sh bd5939745h 1.wav
-$BASE/fs-delete.sh bd5939745h 2.mp3
-$BASE/ts-delete.sh bd5939745h
 $BASE/fs-post.sh bd5939745h 1.wav $BASE/../sample/files/audio.wav
 $BASE/fs-post.sh bd5939745h 2.mp3 $BASE/../sample/files/audio.mp3
 $BASE/ts-put.sh bd5939745h $BASE/../sample/object/formatSampleAudio.rdf.xml
 
 # data (tarball)
-$BASE/fs-delete.sh bd46428055 1.tar.gz
-$BASE/ts-delete.sh bd46428055
 $BASE/fs-post.sh bd46428055 1.tar.gz $BASE/../sample/files/data.tar.gz
 $BASE/ts-put.sh bd46428055 $BASE/../sample/object/formatSampleData.rdf.xml
 
 # image (tif master, jpeg derivatives)
-for fid in 1.tif 2.jpg 3.jpg 4.jpg 5.jpg; do
-	$BASE/fs-delete.sh bd3379993m $fid
-done
-$BASE/ts-delete.sh bd3379993m
 $BASE/fs-post.sh bd3379993m 1.tif $BASE/../sample/files/20775-bb01010101-1-1.tif
 $BASE/ts-put.sh bd3379993m $BASE/../sample/object/formatSampleImage.rdf.xml
 $BASE/fs-derivatives.sh bd3379993m 1.tif
 
 # text (pdf master, jpeg derivatives)
-for fid in 1.pdf 2.jpg 3.jpg 4.jpg 5.jpg; do
-	$BASE/fs-delete.sh bd2083054q $fid
-done
-$BASE/ts-delete.sh bd2083054q
 $BASE/fs-post.sh bd2083054q 1.pdf $BASE/../sample/files/20775-bb01034796-1-1.pdf
 $BASE/ts-put.sh bd2083054q $BASE/../sample/object/formatSampleText.rdf.xml
 $BASE/fs-derivatives.sh bd2083054q 1.pdf
 
 # video (mov master, mp4 and jpeg derivatives)
-for fid in 1.mov 2.mp4 3.jpg 4.jpg 5.jpg; do
-	$BASE/fs-delete.sh bd0786115s $fid
-done
-$BASE/ts-delete.sh bd0786115s
 $BASE/fs-post.sh bd0786115s 1.mov $BASE/../sample/files/video.mov
 $BASE/fs-post.sh bd0786115s 2.mp4 $BASE/../sample/files/video.mp4
 $BASE/fs-post.sh bd0786115s 3.jpg $BASE/../sample/files/video-preview.jpg
--- src/sh/test_XX_formatsComplex.sh
+++ src/sh/test_XX_formatsComplex.sh
@@ -4,23 +4,6 @@
 
 BASE=`dirname $0`
 
-# delete master files and derivatives
-$BASE/fs-delete.sh bd0922518w 1/1.wav # wav master
-$BASE/fs-delete.sh bd0922518w 1/2.mp3 # mp3 service
-$BASE/fs-delete.sh bd0922518w 2/1.tar.gz # data tarball
-for fid in 1.pdf 2.jpg 3.jpg 4.jpg 5.jpg; do # document: pdf master, jpeg derivs
-	$BASE/fs-delete.sh bd0922518w 3/$fid
-done
-for fid in 1.tif 2.jpg 3.jpg 4.jpg 5.jpg; do # image: tif master, jpeg derivs
-	$BASE/fs-delete.sh bd0922518w 4/$fid
-done
-for fid in 1.mov 2.mp4 3.jpg 4.jpg 5.jpg; do # video: mov master, mp4/jpeg derivs
-	$BASE/fs-delete.sh bd0922518w 5/$fid
-done
-
-# delete object metadata
-$BASE/ts-delete.sh bd0922518w
-
 # post master files
 $BASE/fs-post-cmp.sh bd0922518w 1 1.wav $BASE/../sample/files/audio.wav
 $BASE/fs-post-cmp.sh bd0922518w 2 1.tar.gz $BASE/../sample/files/data.tar.gz
